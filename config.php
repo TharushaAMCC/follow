e56ab9f3abca39f8aad5879e38f3857d38de652e
TharushaAMCC <49670773+TharushaAMCC@users.noreply.github.com>
2021-01-22 13:41:46 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$User_Agent = "Mozilla/5.0 (Linux; Android 5.1.1; SM-J120G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.110 Mobile Safari/537.36";
+
+$PHPSESSID = "oglgoi7k5f5t5lhqshhfeojsq6";
+
+
+/* To Get The Password Go From This Link
+=> https://exe.io/Passfollowfast */
+
+$password = "EslamTube3325912";
